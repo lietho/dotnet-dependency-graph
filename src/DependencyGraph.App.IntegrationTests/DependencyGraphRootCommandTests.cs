62421ce017696b3c287a185a4e25911bcd2fb102
Thomas Lielacher <thomas.lielacher@rubicon.eu>
2023-12-31 17:31:24 +0100

    fixed tests

--- src/DependencyGraph.App.IntegrationTests/DependencyGraphRootCommandTests.cs
+++ src/DependencyGraph.App.IntegrationTests/DependencyGraphRootCommandTests.cs
@@ -9,13 +9,13 @@ namespace DependencyGraph.App.IntegrationTests
     {
       get
       {
-        yield return new TestCaseData("DependencyGraph.App.csproj.dgml", new[] { @"..\..\..\..\DependencyGraph.App\DependencyGraph.App.csproj", "-v", "dgml", "-o", "graph.dgml", "--no-restore" });
+        yield return new TestCaseData("DependencyGraph.App.csproj.dgml", new[] { @"../../../../DependencyGraph.App/DependencyGraph.App.csproj", "-v", "dgml", "-o", "graph.dgml", "--no-restore" });
 
-        yield return new TestCaseData("DependencyGraph.App.csproj_Exclude.dgml", new[] { @"..\..\..\..\DependencyGraph.App\DependencyGraph.App.csproj", "-v", "dgml", "-o", "graph.dgml", "-e", "Microsoft.Extensions*", "--no-restore" });
+        yield return new TestCaseData("DependencyGraph.App.csproj_Exclude.dgml", new[] { @"../../../../DependencyGraph.App/DependencyGraph.App.csproj", "-v", "dgml", "-o", "graph.dgml", "-e", "Microsoft.Extensions*", "--no-restore" });
 
-        yield return new TestCaseData("DependencyGraph.App.csproj_Include.dgml", new[] { @"..\..\..\..\DependencyGraph.App\DependencyGraph.App.csproj", "-v", "dgml", "-o", "graph.dgml", "-i", "DependencyGraph*", "*NuGet*", "--no-restore" });
+        yield return new TestCaseData("DependencyGraph.App.csproj_Include.dgml", new[] { @"../../../../DependencyGraph.App/DependencyGraph.App.csproj", "-v", "dgml", "-o", "graph.dgml", "-i", "DependencyGraph*", "*NuGet*", "--no-restore" });
 
-        yield return new TestCaseData("DependencyGraph.App.csproj_MaxDepth.dgml", new[] { @"..\..\..\..\DependencyGraph.App\DependencyGraph.App.csproj", "-v", "dgml", "-o", "graph.dgml", "-d", "2", "--no-restore" });
+        yield return new TestCaseData("DependencyGraph.App.csproj_MaxDepth.dgml", new[] { @"../../../../DependencyGraph.App/DependencyGraph.App.csproj", "-v", "dgml", "-o", "graph.dgml", "-d", "2", "--no-restore" });
       }
     }
 
@@ -24,20 +24,20 @@ namespace DependencyGraph.App.IntegrationTests
     {
       await Program.Main(args);
 
-      CompareFiles("graph.dgml", $"TestFiles\\{testFileName}");
+      CompareFiles("graph.dgml", $"TestFiles/{testFileName}");
     }
 
     public static IEnumerable<TestCaseData> ConsoleTestCases
     {
       get
       {
-        yield return new TestCaseData("DependencyGraph.App.csproj.txt", new[] { @"..\..\..\..\DependencyGraph.App\DependencyGraph.App.csproj", "-v", "console", "--no-restore" });
+        yield return new TestCaseData("DependencyGraph.App.csproj.txt", new[] { @"../../../../DependencyGraph.App/DependencyGraph.App.csproj", "-v", "console", "--no-restore" });
 
-        yield return new TestCaseData("DependencyGraph.App.csproj_Exclude.txt", new[] { @"..\..\..\..\DependencyGraph.App\DependencyGraph.App.csproj", "-v", "console", "-e", "Microsoft.Extensions*", "--no-restore" });
+        yield return new TestCaseData("DependencyGraph.App.csproj_Exclude.txt", new[] { @"../../../../DependencyGraph.App/DependencyGraph.App.csproj", "-v", "console", "-e", "Microsoft.Extensions*", "--no-restore" });
 
-        yield return new TestCaseData("DependencyGraph.App.csproj_Include.txt", new[] { @"..\..\..\..\DependencyGraph.App\DependencyGraph.App.csproj", "-v", "console", "-i", "DependencyGraph*", "*NuGet*", "--no-restore" });
+        yield return new TestCaseData("DependencyGraph.App.csproj_Include.txt", new[] { @"../../../../DependencyGraph.App/DependencyGraph.App.csproj", "-v", "console", "-i", "DependencyGraph*", "*NuGet*", "--no-restore" });
 
-        yield return new TestCaseData("DependencyGraph.App.csproj_MaxDepth.txt", new[] { @"..\..\..\..\DependencyGraph.App\DependencyGraph.App.csproj", "-v", "console", "-d", "2", "--no-restore" });
+        yield return new TestCaseData("DependencyGraph.App.csproj_MaxDepth.txt", new[] { @"../../../../DependencyGraph.App/DependencyGraph.App.csproj", "-v", "console", "-d", "2", "--no-restore" });
       }
     }
 
@@ -60,10 +60,12 @@ namespace DependencyGraph.App.IntegrationTests
         Console.SetOut(consoleOut);
       }
 
-      CompareFiles("consoleOutput.txt", $"TestFiles\\{testFileName}");
+      CompareFiles("consoleOutput.txt", $"TestFiles/{testFileName}");
     }
 
     private static void CompareFiles(string file, string expectedContentFile) => 
-      Assert.That(File.ReadAllText(file), Is.EqualTo(File.ReadAllText(expectedContentFile)));
+      Assert.That(NormalizeNewLines(File.ReadAllText(file)), Is.EqualTo(NormalizeNewLines(File.ReadAllText(expectedContentFile))));
+
+    private static string NormalizeNewLines(string text) => text.Replace("\r\n", Environment.NewLine);
   }
 }
