c3c5f3043a9eea6929d969501b4544502b0baafc
Thomas Lielacher <thomas.lielacher@rubicon.eu>
2025-02-13 17:27:05 +0100

    added TraceDependencyCommand

---
+++ src/DependencyGraph.Core/Diagnostic/GraphDiagnosticBase.cs
@@ -0,0 +1,40 @@
+﻿// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using DependencyGraph.Core.Graph;
+
+namespace DependencyGraph.Core.Diagnostic
+{
+  public abstract class GraphDiagnosticBase<TResult> : IGraphDiagnostic<TResult>
+  {
+    public abstract TResult Perform(IDependencyGraph dependencyGraph);
+
+    protected IEnumerable<IImmutableList<IDependencyGraphNode>> TraverseDepthFirst(IDependencyGraph dependencyGraph)
+    {
+      foreach (var rootNode in dependencyGraph.RootNodes)
+      {
+        var path = ImmutableList.Create(rootNode);
+
+        yield return path;
+
+        foreach (var p in TraverseDepthFirst(rootNode, path))
+          yield return p;
+      }
+    }
+
+    protected IEnumerable<IImmutableList<IDependencyGraphNode>> TraverseDepthFirst(IDependencyGraphNode node, IImmutableList<IDependencyGraphNode> path)
+    {
+      foreach (var child in node.Dependencies)
+      {
+        var newPath = path.Add(child);
+
+        yield return newPath;
+
+        foreach (var p in TraverseDepthFirst(child, newPath))
+          yield return p;
+      }
+    }
+  }
+}
---
+++ src/DependencyGraph.Core/Diagnostic/TraceDependencyGraphDiagnostic.cs
@@ -0,0 +1,40 @@
+﻿// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using DependencyGraph.Core.Graph;
+using NuGet.Versioning;
+
+namespace DependencyGraph.Core.Diagnostic
+{
+  public class TraceDependencyGraphDiagnostic : GraphDiagnosticBase<IEnumerable<IImmutableList<IDependencyGraphNode>>>
+  {
+    private readonly string _namePattern;
+    private readonly VersionRange? _versionRange;
+
+    public TraceDependencyGraphDiagnostic(string namePattern, VersionRange? versionRange)
+    {
+      _namePattern = namePattern;
+      _versionRange = versionRange;
+    }
+
+    public override IEnumerable<IImmutableList<IDependencyGraphNode>> Perform(IDependencyGraph dependencyGraph)
+       => TraverseDepthFirst(dependencyGraph).Where(PathEndsWithSearchedDependency);
+
+    private bool PathEndsWithSearchedDependency(IImmutableList<IDependencyGraphNode> path)
+    {
+      var lastNode = path[path.Count - 1];
+
+      if (_versionRange == null)
+        return Regex.IsMatch(lastNode.Name, _namePattern, RegexOptions.IgnoreCase);
+      else if (lastNode is PackageDependencyGraphNode packageDependencyGraphNode)
+        return Regex.IsMatch(packageDependencyGraphNode.Name, _namePattern, RegexOptions.IgnoreCase) && _versionRange.Satisfies(packageDependencyGraphNode.Version, VersionComparison.Version);
+
+      return false;
+    }
+  }
+}
